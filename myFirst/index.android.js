8fd191fcc3049296bf7e6c8eaa2e385560c741ee
hacktiv8 <hacktiv8@hacktiv8s-Mac-mini-7.local>
2017-05-02 16:21:08 +0700

    add component

--- myFirst/index.android.js
+++ myFirst/index.android.js
@@ -17,7 +17,7 @@ export default class myFirst extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          WASSAAAAP DUUUUDE
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
---
+++ myFirst/src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native'
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return <Text>ASELOLEE</Text>
+  }
+}
+
+export default Main
