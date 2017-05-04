d679009fa418dcf58cac28828447e2d3fc1edaef
andrasat <andra.satria1@gmail.com>
2017-05-04 17:10:50 +0700

    removed handleClick in Link

--- TMDBMovieFinder/src/components/Link.js
+++ TMDBMovieFinder/src/components/Link.js
@@ -3,31 +3,16 @@ import PropTypes from 'prop-types'
 import { Linking, Alert, Button } from 'react-native'
 
 export default class Link extends React.Component {
-
-  handleClick = () => {
-    const { url } = this.props
-
-    if(url === '') {
-      return Alert.alert(
-        'Error',
-        'Something Wrong Happened...')
-    }
-
-    return Linking.canOpenURL(url).then(supported => {
-      supported ? Linking.openURL(url) : Alert.alert(
-        'Error',
-        'Something Wrong Happened...')
-    }).catch(() => {
-      Alert.alert(
-        'Error',
-        'Something Wrong Happened...')
-    })
-  }
-
   render() {
+    const { url } = this.props
     return (
       <Button title="My Github Page"
-        onPress={this.handleClick}
+        onPress={() => {
+          Linking.openURL(url).catch(() => Alert.alert(
+            'Error',
+            'Something wrong...'
+          ))
+        }}
       />
     )
   }
